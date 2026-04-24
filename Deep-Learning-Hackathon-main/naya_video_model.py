b68726940843c3b36e4b2a480d222d20b10620c8
Sahil-Goel1 <159703727+Sahil-Goel1@users.noreply.github.com>
2026-04-24 22:34:09 +0530

    Delete Deep-Learning-Hackathon-main/naya_video_model.py

--- Deep-Learning-Hackathon-main/naya_video_model.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import hf_hub_download
-# Ye model real/fake face classification karta hai
-path = hf_hub_download(
-    repo_id="prithivMLmods/Deep-Fake-Detector-Model",
-    filename="model.safetensors"
-)
